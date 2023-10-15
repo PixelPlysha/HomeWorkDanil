3af73a8e6f1902c0ede2092679bfd1d4c1be8787
Plyshadui <144620464+Plyshadui@users.noreply.github.com>
2023-10-15 16:43:17 -0300

    Add files via upload

---
+++ lesson-78/main.js
@@ -0,0 +1,25 @@
+// const x = 10
+// const y = 20
+
+// console.log(x * y)
+
+
+// 1 string
+// 2 number
+// 3 Boolean
+// 4 undefined
+// 5 null
+// 6 BigInt
+// 7 Symbol
+
+let instrument = 'Инструмент'
+const box = 'Ящик'
+
+// instrument = 10
+// console.log(instrument)
+
+const user = 'User'
+const age = 25
+const isAdmin = true
+
+console.log(`${user} admin: ${isAdmin}`)
