c04e9bbe5df01afba54495e4b71a88089d94c7cb
Plyshadui <144620464+Plyshadui@users.noreply.github.com>
2023-10-15 17:28:45 -0300

    Add files via upload

---
+++ lesson-79/main.js
@@ -0,0 +1,31 @@
+// const firstNumber = 5
+
+// const secondNumber = 10
+
+// let result = null
+
+// if (firstNumber == secondNumber) {
+//     result = firstNumber + secondNumber
+//     console.log(result)
+// } else {
+//     const result = secondNumber - firstNumber
+//     console.log(result)
+// }
+
+
+// const alex = `admin`
+// if (alex === `user`) {
+//     console.log(`Alex is user`)
+// } else if (alex === `vip`) {
+//     console.log(`Alex is vip user`)
+// } else {
+//     console.log(`Alex is admin`)
+// }
+
+// const alex = 'admin'
+
+// alex === 'admin' ? console.log('Alex is admin') : console.log('Alex is not admin')
+
+const age = '18'
+
+age === '18' ? console.log('Вы совершенолетний') : console.log('Вы не совершенолетний')
