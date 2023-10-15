c7d4f3236a7e13f7769a5f09c11d100ca50c4772
Plyshadui <144620464+Plyshadui@users.noreply.github.com>
2023-10-15 18:20:50 -0300

    Add files via upload

---
+++ lesson-80/main.js
@@ -0,0 +1,49 @@
+
+
+
+// let i = 0
+
+// while(i < 10) {
+//     i = i + 1
+//     console.log(i)
+// }
+
+// let i = 0
+
+// do {
+//     i++
+//     console.log(i)
+// } while (i < 5)
+
+// for(let i = 0; i < 10; i++) {
+//     console.log(`Пройден ${i} круг`)
+// }
+
+
+// Домашнее задание
+
+
+
+// for(let i = 2; i <= 12; i = i + 2) {
+//     console.log(`четный ${i} номер`)
+// }
+
+// for(let i = 10; i > 0; i = i - 1) {
+//     console.log(`число ${i}`)
+// }
+
+
+// let i = 2
+
+// while(i <= 12) {
+//     i = i + 2
+//     console.log(i)
+// }
+
+
+let i = 10
+
+while(i > 0) {
+    i = i - 1
+    console.log(i)
+}
