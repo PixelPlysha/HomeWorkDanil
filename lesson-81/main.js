400aa6517ad9b69d7a1f06647e69ad1947248cbe
Plyshadui <144620464+Plyshadui@users.noreply.github.com>
2023-10-16 15:10:19 -0300

    Add files via upload

---
+++ lesson-81/main.js
@@ -0,0 +1,33 @@
+// const numbers = [1, 2, 3, 4, 5]
+
+// // numbers[4] = 6
+
+
+// // console.log(numbers)
+
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i] + 1)
+// }
+
+
+
+// Домашнее задание
+
+// const numbers = [3, 8, 14, 5, 18, 17, 24, 10, 16];
+
+
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] % 2 === 0) {
+//         console.log(numbers[i]);
+//     }
+// }
+
+
+const Colors = ['красный', 'оранжевый', 'желтый', 'зеленый', 'голубой', 'синий', 'фиолетовый'];
+
+for (let i = Colors.length - 1; i >= 0; i--) {
+    console.log(Colors[i]);
+}
+
+
+
